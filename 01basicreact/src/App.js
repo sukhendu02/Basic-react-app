a840b9cc076e10b7bc7f89a2ca1c8aca2a5d6e3d
Sukhendu <sukhendumandal02@gmail.com>
2024-12-17 19:22:17 +0530

    initial commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import Nav from './components/Nav.jsx'
+
+function App() {
+  return (
+   <> 
+   <Nav/>
+   </>
+  );
+}
+
+export default App;
